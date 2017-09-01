88ec1d4daf6ef46e948c7d1e3fb8a3df61788ea6
Pavlo Pohuts <pavlo.pohuts@edvantis.com>
2017-09-01 12:31:10 +0300

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,13 @@
+// var eyes = require('eyes');
+
+
+// PaulL: gruntfile updated following advice from http://www.html5rocks.com/en/tutorials/tooling/supercharging-your-gruntfile/
+// Major change is that individual task config lives in grunt/*.js, rather than in here.  Also
+// introduced parallelism and grunt-newer
+
+/*global module:false*/
+module.exports = function(grunt) {
+
+
+  // Tasks are now registered in the grunt/aliases.js file
+};
---
+++ syncGrid.bat
@@ -0,0 +1,5 @@
+@Echo 'Starting pulling changes from https://github.com/angular-ui/ui-grid.git . . .'
+git pull https://github.com/angular-ui/ui-grid.git --allow-unrelated-histories
+@Echo 'Completed!'
+
+pause
